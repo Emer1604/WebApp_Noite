18f102869db4d6a05f7364a48215de53ef383906
Emer1604 <emerson_ferreira789@outlook.com>
2023-03-31 21:55:31 -0300

    Criação do controller cliente e suas views

---
+++ WebApp_Noite/Controllers/ClienteController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Noite.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        public IActionResult Listar()
+        {
+            return View();
+        }
+    }
+}
