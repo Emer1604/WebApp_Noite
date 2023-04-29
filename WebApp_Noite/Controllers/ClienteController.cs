d76cbecb1e65a56a64e9712269cfdfffba199d16
Emer1604 <emerson_ferreira789@outlook.com>
2023-04-28 22:06:23 -0300

    Criação dos model

--- WebApp_Noite/Controllers/ClienteController.cs
+++ WebApp_Noite/Controllers/ClienteController.cs
@@ -1,20 +1,62 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_Noite.Models;
 
 namespace WebApp_Noite.Controllers
 {
     public class ClienteController : Controller
     {
+        public static List<ClienteModel> db = new List<ClienteModel>();
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult Cadastrar()
         {
-            return View();
+            ClienteModel model = new ClienteModel();
+            return View(model);
         }
         public IActionResult Listar()
         {
-            return View();
+            return View(db);
         }
+
+        [HttpPost]
+        public IActionResult SalvarDados(ClienteModel cliente)
+        {
+            if (cliente.Id == 0)
+            {
+                Random rand = new Random();
+                cliente.Id = rand.Next(1, 9999);
+                db.Add(cliente);
+            }
+            else
+            {
+                int indice = db.FindIndex(a => a.Id == cliente.Id);
+                db[indice] = cliente;
+            }
+            return RedirectToAction("Listar");
+        }
+        public IActionResult Excluir(int id)
+        {
+            ClienteModel item = db.Find(a => a.Id == id);
+            if (item != null)
+            {
+                db.Remove(item);
+            }
+            return RedirectToAction("Listar");
+        }
+        public IActionResult Editar(int id)
+        {
+            ClienteModel item = db.Find(a => a.Id == id);
+            if (item!=null)
+            {
+                return View(item);
+            }
+            else 
+            {
+                return RedirectToAction("Listar");
+            }
+        }
+        
     }
 }
--- WebApp_Noite/Controllers/ProdutoController.cs
+++ WebApp_Noite/Controllers/ProdutoController.cs
@@ -1,16 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_Noite.Models;
 
 namespace WebApp_Noite.Controllers
 {
     public class ProdutoController : Controller
     {
+        public static List<ProdutoModel> dbp = new List<ProdutoModel>();
         public IActionResult Lista()
         {
             return View();
         }
         public IActionResult Cadastrar()
         {
-            return View();
+            ProdutoModel model = new ProdutoModel();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult SalvarDados(ProdutoModel produto)
+        {
+            if (produto.Id == 0)
+            {
+                Random rand = new Random();
+                produto.Id = rand.Next(1, 9999);
+                dbp.Add(produto);
+            }
+            else
+            {
+                int indice = dbp.FindIndex(a => a.Id == produto.Id);
+                dbp[indice] = produto;
+            }
+            return RedirectToAction("Listar");
+        }
+        public IActionResult Excluir(int id)
+        {
+            ProdutoModel item = dbp.Find(a => a.Id == id);
+            if (item != null)
+            {
+                dbp.Remove(item);
+            }
+            return RedirectToAction("Lista");
+        }
+        public IActionResult Editar(int id)
+        {
+            ProdutoModel item = dbp.Find(a => a.Id == id);
+            if (item != null)
+            {
+                return View(item);
+            }
+            else
+            {
+                return RedirectToAction("Lista");
+            }
         }
     }
 }
